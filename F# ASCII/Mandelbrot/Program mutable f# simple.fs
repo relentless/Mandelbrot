fe73e318949f03e789cc12f2fcfde1ab5d72f85e
Grant Crofton <grantac@hotmail.com>
2015-09-23 09:00:19 +0100

    Initial files

---
+++ F# ASCII/Mandelbrot/Program mutable f# simple.fs
@@ -0,0 +1,57 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+let xMinScale = -2.5
+let xMaxScale = 1.0
+
+let yMinScale = -1.0
+let yMaxScale = 1.0
+
+let xMinOuput = 0.0
+let xMaxOutput = 79.0
+
+let yMinOutput = 0.0
+let yMaxOutput = 24.0
+
+let max_iterations = 1000
+
+let scale inputMin inputMax outputMin outputMax number =
+    let onePercent = (number - inputMin) / (inputMax - inputMin)
+    let scaled = onePercent * (outputMax - outputMin) + outputMin
+    scaled
+
+let scaleX number =
+    number |> scale xMinOuput xMaxOutput xMinScale xMaxScale 
+
+let scaleY number =
+    number |> scale yMinOutput yMaxOutput yMinScale yMaxScale
+
+[<EntryPoint>]
+let main argv = 
+    
+    for pY in 0.0..yMaxOutput do
+        for pX in 0.0..xMaxOutput do
+            let x0 = pX |> scaleX
+            let y0 = pY |> scaleY
+
+            let mutable x = 0.0
+            let mutable y = 0.0
+
+            let mutable iteration = 0
+
+            while x*x + y*y < 2.0*2.0 && iteration < max_iterations do
+              
+                let xtemp = x*x - y*y + x0
+                y <- 2.0*x*y + y0
+                x <- xtemp
+                iteration <- iteration + 1
+              
+            let output = 
+                match iteration with
+                | 1000 -> " "
+                | _ -> "*"
+            
+            printf "%s" output
+            
+    System.Console.ReadLine() |> ignore
+    0 // return an integer exit code
---
+++ F# ASCII/Mandelbrot/Program.fs
@@ -0,0 +1,61 @@
+﻿open System
+open System.Drawing
+
+let xMinMandelbrotCoord = -2.5
+let xMaxMandebrotCoord = 1.0
+
+let yMinMandelbrotCoord = -1.0
+let yMaxMandelbrotCoord = 1.0
+
+let xOutputPixels = 179.0 // console width in characters.  default seems to be 80.  Set to n-1
+let yOutputPixels = 59.0 // console height in lines. Default seems to be 25.  Set to n-1
+
+let maxIterations = 20 // iterations to try before giving up
+
+let scale inputMax outputMin outputMax number =
+    let onePercent = number / inputMax
+    onePercent * (outputMax - outputMin) + outputMin
+
+type Range = {from: float; upto: float }
+
+let scale' inputRange outputRange number =
+    let onePercent = (number - inputRange.from ) / (inputRange.upto - inputRange.from)
+    onePercent * (outputRange.upto - outputRange.from) + outputRange.from
+
+let outputs = [| " "; "."; "+"; "o"; "x"; "*"; "#"; "%"; "$"; "@" |] |> Array.rev
+
+let colours = [| ConsoleColor.Black; ConsoleColor.Yellow; ConsoleColor.Cyan; ConsoleColor.Red; ConsoleColor.DarkGreen |] |> Array.rev
+
+let withinRadius radius (x, y) = x*x + y*y < radius*radius
+
+[<EntryPoint>]
+let main _ = 
+    
+    Console.SetWindowSize(int xOutputPixels + 1, int yOutputPixels + 1);
+    Console.SetWindowPosition(0,0)
+
+    for yPixel in 0.0..yOutputPixels do
+        for xPixel in 0.0..xOutputPixels do
+
+            let xScaled = xPixel |> scale' {from=0.0; upto=xOutputPixels} {from=xMinMandelbrotCoord; upto=xMaxMandebrotCoord} 
+            let yScaled = yPixel |> scale yOutputPixels yMinMandelbrotCoord yMaxMandelbrotCoord
+
+            let rec applyOperation iteration x y =
+                if (x,y) |> withinRadius 2.0 && iteration < maxIterations then
+                    let nextX = x*x - y*y + xScaled
+                    let nextY = 2.0*x*y + yScaled
+                    applyOperation (iteration + 1) nextX nextY
+                else
+                    let outputIndex = (float iteration) |> scale (float maxIterations) 0.0 (float outputs.Length - 1.0) |> int 
+                    let outputString = outputs.[outputIndex]
+
+                    let outputColourIndex = (float iteration) |> scale (float maxIterations) 0.0 (float colours.Length - 1.0) |> int 
+                    let outputColour = colours.[outputColourIndex]
+            
+                    Console.ForegroundColor <- outputColour
+                    printf "%s" outputString
+
+            applyOperation 0 0.0 0.0
+            
+    System.Console.ReadLine() |> ignore
+    0
