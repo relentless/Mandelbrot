059ef601b170ad6a99a5bf6d7c8e33b435bde6ef
Grant Crofton <grantac@hotmail.com>
2015-09-24 15:18:46 +0100

    Added more colours, tidied up

--- F# ASCII/Mandelbrot/Program.fs
+++ F# ASCII/Mandelbrot/Program.fs
@@ -2,7 +2,7 @@
 open System.Drawing
 
 let xMinMandelbrotCoord = -2.5
-let xMaxMandebrotCoord = 1.0
+let xMaxMandelbrotCoord = 1.0
 
 let yMinMandelbrotCoord = -1.0
 let yMaxMandelbrotCoord = 1.0
@@ -12,48 +12,66 @@ let yOutputPixels = 59.0 // console height in lines. Default seems to be 25.  Se
 
 let maxIterations = 20 // iterations to try before giving up
 
-let scale inputMax outputMin outputMax number =
-    let onePercent = number / inputMax
-    onePercent * (outputMax - outputMin) + outputMin
+// character to be printed at each pixel, depending on iteration count
+let characters = [| " ";  "+"; "x"; "*"; "#"; "%"; "$"; "@" |] |> Array.rev
 
-type Range = {from: float; upto: float }
+// colour of character to be printed at each pixel, depending on iteration count
+let colours = [|
+    ConsoleColor.Black; 
+    ConsoleColor.White; 
+    ConsoleColor.Yellow; 
+    ConsoleColor.Gray; 
+    ConsoleColor.Cyan; 
+    ConsoleColor.Green; 
+    ConsoleColor.Magenta; 
+    ConsoleColor.Red; 
+    ConsoleColor.Blue;  
+    ConsoleColor.DarkYellow; 
+    ConsoleColor.DarkGray; 
+    ConsoleColor.DarkCyan; 
+    ConsoleColor.DarkGreen; 
+    ConsoleColor.DarkMagenta; 
+    ConsoleColor.DarkRed; 
+    ConsoleColor.DarkBlue; |] |> Array.rev
 
-let scale' inputRange outputRange number =
-    let onePercent = (number - inputRange.from ) / (inputRange.upto - inputRange.from)
-    onePercent * (outputRange.upto - outputRange.from) + outputRange.from
+type Scale = {inputRange:float*float; outputRange:float*float}
+let upto b a = (a,b)
 
-let outputs = [| " "; "."; "+"; "o"; "x"; "*"; "#"; "%"; "$"; "@" |] |> Array.rev
-
-let colours = [| ConsoleColor.Black; ConsoleColor.Yellow; ConsoleColor.Cyan; ConsoleColor.Red; ConsoleColor.DarkGreen |] |> Array.rev
+// take a number from one range and scale it to a different range
+let scale ranges number =
+    let min (a,b) = a
+    let max (a,b) = b
+    let onePercent = (number - min ranges.inputRange) / (max ranges.inputRange - min ranges.inputRange)
+    onePercent * (max ranges.outputRange - min ranges.outputRange) + min ranges.outputRange
 
 let withinRadius radius (x, y) = x*x + y*y < radius*radius
+
+// returns one of the outputs as relative to iteration against maxIterations
+let selectOutput iteration (outputs:'a array) =
+    let outputIndex = (float iteration) |> scale {inputRange = 0.0 |> upto (float maxIterations); outputRange = 0.0 |> upto (float outputs.Length - 1.0)} |> int
+    outputs.[outputIndex]
 
 [<EntryPoint>]
 let main _ = 
     
     Console.SetWindowSize(int xOutputPixels + 1, int yOutputPixels + 1);
-    Console.SetWindowPosition(0,0)
 
     for yPixel in 0.0..yOutputPixels do
         for xPixel in 0.0..xOutputPixels do
 
-            let xScaled = xPixel |> scale' {from=0.0; upto=xOutputPixels} {from=xMinMandelbrotCoord; upto=xMaxMandebrotCoord} 
-            let yScaled = yPixel |> scale yOutputPixels yMinMandelbrotCoord yMaxMandelbrotCoord
+            let xScaled = xPixel |> scale {inputRange = 0.0 |> upto xOutputPixels; outputRange = xMinMandelbrotCoord |> upto xMaxMandelbrotCoord}
+            let yScaled = yPixel |> scale {inputRange = 0.0 |> upto yOutputPixels; outputRange = yMinMandelbrotCoord |> upto yMaxMandelbrotCoord}
 
             let rec applyOperation iteration x y =
-                if (x,y) |> withinRadius 2.0 && iteration < maxIterations then
+                if (x,y) |> withinRadius 2.0 && 
+                   iteration < maxIterations
+                then
                     let nextX = x*x - y*y + xScaled
                     let nextY = 2.0*x*y + yScaled
                     applyOperation (iteration + 1) nextX nextY
                 else
-                    let outputIndex = (float iteration) |> scale (float maxIterations) 0.0 (float outputs.Length - 1.0) |> int 
-                    let outputString = outputs.[outputIndex]
-
-                    let outputColourIndex = (float iteration) |> scale (float maxIterations) 0.0 (float colours.Length - 1.0) |> int 
-                    let outputColour = colours.[outputColourIndex]
-            
-                    Console.ForegroundColor <- outputColour
-                    printf "%s" outputString
+                    Console.ForegroundColor <- selectOutput iteration colours
+                    printf "%s" (selectOutput iteration characters)
 
             applyOperation 0 0.0 0.0
             
